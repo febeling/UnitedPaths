9c0d2ca3a9cceb9903c943596fa71cfa5d510190
Florian Ebeling <florian.ebeling@gmail.com>
2012-02-08 19:50:36 +0100

    Extend the test app

--- BezierLab/FLDocument.h
+++ BezierLab/FLDocument.h
@@ -3,30 +3,36 @@
 //  BezierLab
 //
 //  Created by Florian Ebeling on 24.01.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 40lines. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 #import "FLCanvas.h"
+#import "NSBezierPath+BezierLabs.h"
 
 @interface FLDocument : NSDocument
 {
   FLCanvas *canvas;
-  NSArray *shapes;
+  NSMutableArray *shapes;
   NSArray *colors;
 }
 
-- (IBAction)setAndOperation:(id)sender;
-- (IBAction)resetOperation:(id)sender;
+- (IBAction)performUnionOperation:(id)sender;
 
 - (IBAction)clear:(id)sender;
 - (IBAction)setTwoRectangles:(id)sender;
 - (IBAction)setTwoRoundedRectangles:(id)sender;
 - (IBAction)setKeyboardShapes:(id)sender;
 - (IBAction)setFlatnessShapes:(id)sender;
+- (IBAction)setTShapeRectangles:(id)sender;
+- (IBAction)setCircleOverSquareCorner:(id)sender;
+- (IBAction)setCircleOverSquareSide:(id)sender;
+- (IBAction)setSquareOverCircleCorner:(id)sender;
+- (IBAction)setSquareOverCircleSide:(id)sender;
+- (IBAction)setSquareNextToSquare:(id)sender;
 
 @property (strong) IBOutlet FLCanvas *canvas;
-@property (strong) NSArray *shapes;
+@property (strong) NSMutableArray *shapes;
 @property (readonly) NSAttributedString *pathDescription;
 
 @end
