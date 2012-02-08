d996493407ef0c62e188a9c7ef1c4c7f379dd891
Florian Ebeling <florian.ebeling@gmail.com>
2012-02-08 19:50:36 +0100

    Extend the test app

--- BezierLab/FLDocument.m
+++ BezierLab/FLDocument.m
@@ -16,8 +16,7 @@
 {
   self = [super init];
   if (self) {
-    // Add your subclass-specific initialization here.
-    // If an error occurs here, return nil.
+    shapes = [NSMutableArray array];
   }
   return self;
 }
@@ -62,100 +61,6 @@
   [canvas bind:@"shapes" toObject:self withKeyPath:@"shapes" options:nil];
 }
 
-- (IBAction)setAndOperation:(id)sender;
-{
-  
-}
-
-- (IBAction)resetOperation:(id)sender;
-{
-  
-}
-
-- (IBAction)clear:(id)sender;
-{
-  self.shapes = [NSArray array];
-}
-
-- (IBAction)setTwoRectangles:(id)sender;
-{
-  NSDictionary *rect1 = [NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRect:NSMakeRect(100, 100, 200, 100)], @"path",
-                         [self colorWithIndex:1], @"color", nil];
-  NSDictionary *rect2 = [NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRect:NSMakeRect(150, 150, 200, 100)], @"path",
-                         [self colorWithIndex:4], @"color", nil];
-  self.shapes = [NSArray arrayWithObjects:rect1, rect2, nil];
-}
-
-- (IBAction)setTwoRoundedRectangles:(id)sender;
-{
-  NSDictionary *rect1 = [NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRoundedRect:NSMakeRect(100, 100, 200, 100)
-                                                                                                   xRadius:20
-                                                                                                   yRadius:20], @"path",
-                         [self colorWithIndex:0], @"color", nil];
-  NSDictionary *rect2 = [NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRoundedRect:NSMakeRect(150, 150, 200, 100)
-                                                                                                   xRadius:20
-                                                                                                   yRadius:20], @"path",
-                         [self colorWithIndex:2], @"color", nil];
-  self.shapes = [NSArray arrayWithObjects:rect1, rect2, nil];
-}
-
-- (IBAction)setKeyboardShapes:(id)sender
-{
-  NSMutableArray *newShapes = [NSMutableArray array];
-  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRoundedRect:NSMakeRect(50, 250, 100, 100)
-                                                                                                   xRadius:20
-                                                                                                   yRadius:20], @"path",
-                         [self colorWithIndex:0], @"color", nil]];
-  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRoundedRect:NSMakeRect(180, 250, 100, 100)
-                                                                                                   xRadius:20
-                                                                                                   yRadius:20], @"path",
-                         [self colorWithIndex:1], @"color", nil]];
-  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRoundedRect:NSMakeRect(100, 120, 100, 100)
-                                                                                                   xRadius:20
-                                                                                                   yRadius:20], @"path",
-                         [self colorWithIndex:2], @"color", nil]];
-  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRect:NSMakeRect(100, 250, 130, 100)], @"path",
-                         [self colorWithIndex:3], @"color", nil]];
-  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRect:NSMakeRect(100, 170, 100, 130)], @"path",
-                         [self colorWithIndex:4], @"color", nil]];
-  
-  self.shapes = newShapes;
-}
-
-- (IBAction)setFlatnessShapes:(id)sender
-{
-  NSMutableArray *newShapes = [NSMutableArray array];
-  NSBezierPath *path = [NSBezierPath bezierPath];
-  [path moveToPoint:NSMakePoint(30,30)];
-  [path curveToPoint:NSMakePoint(260,260)
-       controlPoint1:NSMakePoint(30,210)
-       controlPoint2:NSMakePoint(80,260)];
-  [path lineToPoint:NSMakePoint(260,30)];
-  [path closePath];
-
-  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:path, @"path",
-                        [self colorWithIndex:0], @"color", nil]];
-  
-  NSAffineTransform *transform = [NSAffineTransform transform];
-  [transform translateXBy:30 yBy:30];
-
-  [NSBezierPath setDefaultFlatness:2];
-  NSBezierPath *rough = [path bezierPathByFlatteningPath];
-  [rough transformUsingAffineTransform:transform];
-  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:rough, @"path",
-                        [self colorWithIndex:3], @"color", nil]];
-
-  [NSBezierPath setDefaultFlatness:10];
-  NSBezierPath *fine = [path bezierPathByFlatteningPath];
-  [fine transformUsingAffineTransform:transform];
-  [fine transformUsingAffineTransform:transform];
-  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:fine, @"path",
-                        [self colorWithIndex:5], @"color", nil]];
-  [NSBezierPath setDefaultFlatness:0.6];
-  
-  self.shapes = newShapes;
-}
-
 - (NSString *)windowNibName
 {
   // Override returning the nib file name of the document
@@ -175,9 +80,9 @@
    Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
    You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
    */
-  NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-  @throw exception;
-  return nil;
+//  NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
+//  @throw exception;
+//  return nil;
 }
 
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
@@ -187,14 +92,175 @@
    You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
    If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
    */
-  NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-  @throw exception;
+//  NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
+//  @throw exception;
   return YES;
 }
 
 + (BOOL)autosavesInPlace
 {
   return YES;
+}
+
+#pragma mark Actions
+
+- (IBAction)performUnionOperation:(id)sender
+{
+  if([self.shapes count] < 2) return;
+  
+  NSDictionary *first = [self.shapes objectAtIndex:0];
+  NSDictionary *second = [self.shapes objectAtIndex:1];
+  
+  NSMutableDictionary *newShape = [NSMutableDictionary dictionary];
+  [newShape setObject:[first objectForKey:@"color"] forKey:@"color"];
+  
+  NSBezierPath *firstPath = [first objectForKey:@"path"];
+  NSBezierPath *secondPath = [second objectForKey:@"path"];
+  
+  NSBezierPath *newPath = [firstPath bezierPathByUnionWith:secondPath];
+  [newShape setObject:newPath forKey:@"path"];
+  
+  NSMutableArray *newShapeArray = [NSMutableArray array];
+  [newShapeArray addObject:newShape];
+  [newShapeArray addObjectsFromArray:[self.shapes subarrayWithRange:NSMakeRange(2,[self.shapes count]-2)]];
+  
+  self.shapes = newShapeArray;
+}
+
+- (IBAction)clear:(id)sender;
+{
+  self.shapes = [NSMutableArray array];
+}
+
+- (IBAction)setTwoRectangles:(id)sender;
+{
+  NSDictionary *rect1 = [NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRect:NSMakeRect(100, 100, 200, 100)], @"path",
+                         [self colorWithIndex:1], @"color", nil];
+  NSDictionary *rect2 = [NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRect:NSMakeRect(150, 150, 200, 100)], @"path",
+                         [self colorWithIndex:4], @"color", nil];
+  
+  NSMutableArray *shapesProxy = [self mutableArrayValueForKey:@"shapes"];
+  
+  [shapesProxy removeAllObjects];
+  [shapesProxy addObject:rect1];
+  [shapesProxy addObject:rect2];
+}
+
+- (IBAction)setTwoRoundedRectangles:(id)sender;
+{
+  NSDictionary *rect1 = [NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRoundedRect:NSMakeRect(100, 100, 200, 100)
+                                                                                                   xRadius:20
+                                                                                                   yRadius:20], @"path",
+                         [self colorWithIndex:0], @"color", nil];
+  NSDictionary *rect2 = [NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRoundedRect:NSMakeRect(150, 150, 200, 100)
+                                                                                                   xRadius:20
+                                                                                                   yRadius:20], @"path",
+                         [self colorWithIndex:2], @"color", nil];
+
+  NSMutableArray *shapesProxy = [self mutableArrayValueForKey:@"shapes"];
+  
+  [shapesProxy removeAllObjects];
+  [shapesProxy addObject:rect1];
+  [shapesProxy addObject:rect2];
+}
+
+- (IBAction)setKeyboardShapes:(id)sender
+{
+  NSMutableArray *newShapes = [NSMutableArray array];
+  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRoundedRect:NSMakeRect(50, 250, 100, 100)
+                                                                                                  xRadius:20
+                                                                                                  yRadius:20], @"path",
+                        [self colorWithIndex:0], @"color", nil]];
+  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRoundedRect:NSMakeRect(180, 250, 100, 100)
+                                                                                                  xRadius:20
+                                                                                                  yRadius:20], @"path",
+                        [self colorWithIndex:1], @"color", nil]];
+  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRoundedRect:NSMakeRect(100, 120, 100, 100)
+                                                                                                  xRadius:20
+                                                                                                  yRadius:20], @"path",
+                        [self colorWithIndex:2], @"color", nil]];
+  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRect:NSMakeRect(100, 250, 130, 100)], @"path",
+                        [self colorWithIndex:3], @"color", nil]];
+  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRect:NSMakeRect(100, 170, 100, 130)], @"path",
+                        [self colorWithIndex:4], @"color", nil]];
+  
+  self.shapes = newShapes;
+}
+
+- (IBAction)setFlatnessShapes:(id)sender
+{
+  NSMutableArray *newShapes = [NSMutableArray array];
+  NSBezierPath *path = [NSBezierPath bezierPath];
+  [path moveToPoint:NSMakePoint(30,30)];
+  [path curveToPoint:NSMakePoint(260,260)
+       controlPoint1:NSMakePoint(30,210)
+       controlPoint2:NSMakePoint(80,260)];
+  [path lineToPoint:NSMakePoint(260,30)];
+  [path closePath];
+  
+  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:path, @"path",
+                        [self colorWithIndex:0], @"color", nil]];
+  
+  NSAffineTransform *transform = [NSAffineTransform transform];
+  [transform translateXBy:30 yBy:30];
+  
+  [NSBezierPath setDefaultFlatness:2];
+  NSBezierPath *rough = [path bezierPathByFlatteningPath];
+  [rough transformUsingAffineTransform:transform];
+  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:rough, @"path",
+                        [self colorWithIndex:3], @"color", nil]];
+  
+  [NSBezierPath setDefaultFlatness:10];
+  NSBezierPath *fine = [path bezierPathByFlatteningPath];
+  [fine transformUsingAffineTransform:transform];
+  [fine transformUsingAffineTransform:transform];
+  [newShapes addObject:[NSDictionary dictionaryWithObjectsAndKeys:fine, @"path",
+                        [self colorWithIndex:5], @"color", nil]];
+  [NSBezierPath setDefaultFlatness:0.6];
+  
+  self.shapes = newShapes;
+}
+
+- (IBAction)setTShapeRectangles:(id)sender
+{
+  NSDictionary *square = [NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRect:NSMakeRect(50, 50, 150, 150)], @"path",
+                         [self colorWithIndex:0], @"color", nil];
+  NSDictionary *circle = [NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithOvalInRect:NSMakeRect(125, 50, 150, 150)], @"path",
+                         [self colorWithIndex:2], @"color", nil];
+  
+  NSMutableArray *shapesProxy = [self mutableArrayValueForKey:@"shapes"];
+  
+  [shapesProxy removeAllObjects];
+  [shapesProxy addObject:square];
+  [shapesProxy addObject:circle];
+}
+
+- (IBAction)setCircleOverSquareCorner:(id)sender
+{
+
+}
+
+- (IBAction)setCircleOverSquareSide:(id)sender
+{}
+
+- (IBAction)setSquareOverCircleCorner:(id)sender
+{}
+
+- (IBAction)setSquareOverCircleSide:(id)sender
+{}
+
+- (IBAction)setSquareNextToSquare:(id)sender
+{
+  NSDictionary *square = [NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRect:NSMakeRect(50, 50, 150, 150)], @"path",
+                          [self colorWithIndex:4], @"color", nil];
+  NSDictionary *square2 = [NSDictionary dictionaryWithObjectsAndKeys:[NSBezierPath bezierPathWithRect:NSMakeRect(125, 50, 150, 150)], @"path",
+                          [self colorWithIndex:5], @"color", nil];
+  
+  NSMutableArray *shapesProxy = [self mutableArrayValueForKey:@"shapes"];
+  
+  [shapesProxy removeAllObjects];
+  [shapesProxy addObject:square];
+  [shapesProxy addObject:square2];
 }
 
 @end
