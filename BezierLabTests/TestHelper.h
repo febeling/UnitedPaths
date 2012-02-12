1abf4e87a1b3e0c16712be98bf41f00a4ef605ed
Florian Ebeling <florian.ebeling@gmail.com>
2012-02-12 11:29:39 +0100

    Sort test files in project, indent test helper

--- BezierLabTests/TestHelper.h
+++ BezierLabTests/TestHelper.h
@@ -9,12 +9,12 @@
 #ifndef BezierLab_TestHelper_h
 #define BezierLab_TestHelper_h
 
-#define AssertPointsEqualWithAccuracy(P1, P2, D)                  \
-{                                                               \
-NSPoint __p1 = (P1);                                          \
-NSPoint __p2 = (P2);                                          \
-STAssertEqualsWithAccuracy(__p1.x, __p2.x, D, @"x of point"); \
-STAssertEqualsWithAccuracy(__p1.y, __p2.y, D, @"y of point"); \
+#define AssertPointsEqualWithAccuracy(P1, P2, D)                    \
+{                                                                   \
+  NSPoint __p1 = (P1);                                              \
+  NSPoint __p2 = (P2);                                              \
+  STAssertEqualsWithAccuracy(__p1.x, __p2.x, D, @"x of point");     \
+  STAssertEqualsWithAccuracy(__p1.y, __p2.y, D, @"y of point");     \
 }
 
 #endif
