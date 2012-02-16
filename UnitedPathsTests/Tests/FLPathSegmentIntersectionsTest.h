0ae23690df73286a65ae803f9e8e623006292549
Florian Ebeling <florian.ebeling@gmail.com>
2012-02-16 12:11:08 +0100

    Purge boilerplate comment from test headers

--- UnitedPathsTests/Tests/FLPathSegmentIntersectionsTest.h
+++ UnitedPathsTests/Tests/FLPathSegmentIntersectionsTest.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2012 40lines. All rights reserved.
 //
 
-//  Logic unit tests contain unit test code that is designed to be linked into an independent test executable.
-
 #import <SenTestingKit/SenTestingKit.h>
 
 @interface FLPathSegmentIntersectionsTest : SenTestCase
